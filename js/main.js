e276a833477e95d00698d523301153308870d5c2
Shunji Itoh <notfourch@gmail.com>
2026-08-06 18:31:40 +0900

    Add base map layer control

--- js/main.js
+++ js/main.js
@@ -1,25 +1,40 @@
-        // 地図を作成
-        // 初期位置：古座川町・串本町付近
-        const map = L.map("map").setView(
-            [33.52, 135.72],
-            11
-        );
+// 地図を作成
+const map = L.map("map").setView(
+    [33.52, 135.72],
+    11
+);
 
-        // 地理院標準地図を表示
-        const gsiStandard = L.tileLayer(
-            "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png",
-            {
-                attribution:
-                    '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">地理院タイル</a>',
-                minZoom: 5,
-                maxZoom: 18
-            }
-        );
+// 地理院標準地図
+const gsiStandard = L.tileLayer(
+    "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png",
+    {
+        attribution:
+            '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">地理院タイル</a>',
+        minZoom: 5,
+        maxZoom: 18
+    }
+);
 
-        gsiStandard.addTo(map);
+// 初期背景地図として表示
+gsiStandard.addTo(map);
 
-        // 縮尺を表示
-        L.control.scale({
-            imperial: false,
-            metric: true
-        }).addTo(map);
+// 背景地図の一覧
+const baseMaps = {
+    "地理院標準地図": gsiStandard
+};
+
+// レイヤー切替コントロールを表示
+L.control.layers(
+    baseMaps,
+    null,
+    {
+        collapsed: false,
+        position: "topright"
+    }
+).addTo(map);
+
+// 縮尺を表示
+L.control.scale({
+    imperial: false,
+    metric: true
+}).addTo(map);
